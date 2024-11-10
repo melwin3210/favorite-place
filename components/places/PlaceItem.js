2f121c69d481e75b33e049354cb93ecbb7f20c42
melwin <melwin3210@gmail.com>
2024-11-10 20:26:01 +0530

    places list items passed

--- components/places/PlaceItem.js
+++ components/places/PlaceItem.js
@@ -1,20 +1,55 @@
-import React from 'react'
-import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { Colors } from "../../constants/colors";
 
-const PlaceItem = ({place, onSelect}) => {
+const PlaceItem = ({ place, onSelect }) => {
   return (
-    <Pressable onPress={onSelect}>
-        <Image source={{uri:place.imageUrl}} />
-        <View>
-            <Text>{place.title}</Text>
-            <Text>{place.address}</Text>
-        </View>
+    <Pressable onPress={onSelect} style={({pressed})=>[styles.item, pressed && styles.pressed]}>
+      <Image source={{ uri: place.imageUri }} style={styles.image} />
+      <View style={styles.info}>
+        <Text style={styles.text}>{place.title}</Text>
+        <Text style={styles.address}>{place.address}</Text>
+      </View>
     </Pressable>
-  )
-}
+  );
+};
 
 export default PlaceItem;
 
 const styles = StyleSheet.create({
-    
-})
+  item: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    borderRadius: 4,
+    backgroundColor: Colors.primary500,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.15,
+    shadowOffset: { width: 1, height: 1 },
+    shadowRadius: 2,
+    borderRadius: 4,
+  },
+  pressed: {
+    opacity: 0.9,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: Colors.gray700,
+  },
+  address: {
+    fontSize: 12,
+    color: Colors.gray700,
+  },
+  image: {
+    flex: 1,
+    height: 100,
+    margin:4,
+    borderTopLeftRadius: 4,
+    borderBottomLeftRadius: 4,
+  },
+  info: {
+    flex: 2,
+    padding:12
+  },
+});
